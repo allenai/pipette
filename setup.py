46330e2428b1e070d3cb4dc6751527b3e129481a
Dirk Groeneveld <dirkg@allenai.org>
2019-05-29 11:42:40 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='pipette',
-  version='1.0.0',
+  version='1.1.0',
   description='Python framework for managing experiment workflows',
   url='https://github.com/allenai/pipette',
   packages=find_packages(),
